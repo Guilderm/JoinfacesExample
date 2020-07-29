cb4aa254657047c9d24d39294f02dc877ef30685
Guilder W. Milliner <guilderw@live.com>
2020-07-28 21:43:53 -0600

    Initial commit

---
+++ src/main/java/com/example/helloworld/BootsFacesExample.java
@@ -0,0 +1,13 @@
+package com.example.helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootsFacesExample {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootsFacesExample.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/helloworld/controllers/WebPageController.java
@@ -0,0 +1,12 @@
+package com.example.helloworld.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WebPageController {
+    @GetMapping("/")
+    public String homePage() {
+        return "\\index.xhtml";
+    }
+}
